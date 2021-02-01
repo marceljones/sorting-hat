d8fdb689db241a20efa0aceb72b514bb20068c10
marceljones <marceljones.mj@gmail.com>
2021-02-01 02:09:18 -0600

    setup data structure

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
 console.log("The best things in life are yet to come.")
+
+const houses = [
+  {
+    name: '',
+    image: ''
+  },
+
+  {
+    name: '',
+    image: ''
+  },
+
+  {
+    name: '',
+    image: ''
+  },
+
+  {
+    name: '',
+    image: ''
+  }
+]
+
+//List of student names
+// Harry Potter
+// Ron Weasley
+// Hermione Granger
+// Colin Creevey
+// Seamus Finnigan
+// Hannah Abbott
+// Pansy Parkinson
+// Ernie Macmillan
+// Vincent Crabbe
+// Gregory Goyle
+// Lavender Brown
+// Katie Bell
+// Parvati Patil
+// Cormac McLaggen
+// Neville Longbottom
+// Ginny Weasley
+
+// links for pics  https://harrypotter.neoseeker.com/wiki/List_of_students_that_go_to_Hogwarts
